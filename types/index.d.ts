8f04bd25f1575e056a95c679cddd8864b81fe8fe
Wark0711 <mickey.rahul98@gmail.com>
2025-04-05 16:27:28 +0530

    Started working on home layout and interview cards layout

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,37 @@
 type FormType = "sign-in" | "sign-up";
+
+interface Interview {
+    id: string;
+    role: string;
+    level: string;
+    questions: string[];
+    techstack: string[];
+    createdAt: string;
+    userId: string;
+    type: string;
+    finalized: boolean;
+}
+
+interface InterviewCardProps {
+    interviewId?: string;
+    userId?: string;
+    role: string;
+    type: string;
+    techstack: string[];
+    createdAt?: string;
+}
+
+interface Feedback {
+    id: string;
+    interviewId: string;
+    totalScore: number;
+    categoryScores: Array<{
+        name: string;
+        score: number;
+        comment: string;
+    }>;
+    strengths: string[];
+    areasForImprovement: string[];
+    finalAssessment: string;
+    createdAt: string;
+}
